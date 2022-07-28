646c38928c9cb39895dd2466e92cc8da7dec2a94
Markus <Markus@MarkusVM.localdomain>
2022-07-28 10:49:59 +0200

    ...

--- Glamazon/src/view/kasse/KassePanel.java
+++ Glamazon/src/view/kasse/KassePanel.java
@@ -20,6 +20,7 @@ public class KassePanel extends JPanel{
 	private JTextField txtNr;
 	private JButton btnBackToCart;
 	private JButton btnSendOrder;
+	private JLabel lblSumme;
 	
 	public JPanel getHeaderPanel() {
 		return headerPanel;
@@ -81,7 +82,12 @@ public class KassePanel extends JPanel{
 	public void setBtnSendOrder(JButton btnSendOrder) {
 		this.btnSendOrder = btnSendOrder;
 	}
-	
+	public JLabel getLblSumme() {
+		return lblSumme;
+	}
+	public void setLblSumme(JLabel lblSumme) {
+		this.lblSumme = lblSumme;
+	}
 	public KassePanel() {
 		this.setBackground(Color.LIGHT_GRAY);
 		this.setBounds(0, 150, 1008, 580);
@@ -103,7 +109,73 @@ public class KassePanel extends JPanel{
 		this.getDataPanel().setLayout(null);
 		this.add(this.getDataPanel());
 		
+		JLabel lblVersaddaten = new JLabel("Versanddaten");
+		lblVersaddaten.setBounds(10, 11, 130, 31);
+		this.getDataPanel().add(lblVersaddaten);
 		
+		this.setTxtVorname(new JTextField(10));
+		this.getTxtVorname().setBounds(10, 97, 200, 30);
+		this.getDataPanel().add(this.getTxtVorname());
+		
+		this.setTxtNachname(new JTextField(10));
+		this.getTxtNachname().setBounds(10, 161, 200, 30);
+		this.getDataPanel().add(this.getTxtNachname());
+		
+		this.setTxtStrasse(new JTextField(10));
+		this.getTxtStrasse().setBounds(10, 225, 200, 30);
+		this.getDataPanel().add(this.getTxtStrasse());
+		
+		this.setTxtNr(new JTextField(10));
+		this.getTxtNr().setBounds(220, 225,75,30);
+		this.getDataPanel().add(this.getTxtNr());
+		
+		this.setTxtPlz(new JTextField(10));
+		this.getTxtPlz().setBounds(10, 285, 75, 30);
+		this.getDataPanel().add(this.getTxtPlz());
+		
+		this.setTxtOrt(new JTextField(10));
+		this.getTxtOrt().setBounds(95, 285, 200, 30);
+		this.getDataPanel().add(this.getTxtOrt());
+		
+		JLabel lblVorname = new JLabel("Vorname");
+		lblVorname.setBounds(10, 73, 89, 23);
+		this.getDataPanel().add(lblVorname);
+		
+		JLabel lblNachname = new JLabel("Nachname");
+		lblNachname.setBounds(10, 138, 89, 23);
+		this.getDataPanel().add(lblNachname);
+		
+		JLabel lblStrasse = new JLabel("Strasse");
+		lblStrasse.setBounds(10, 202, 89, 23);
+		this.getDataPanel().add(lblStrasse);
+		
+		JLabel lblNr = new JLabel("Nr.");
+		lblNr.setBounds(220, 202, 75, 23);
+		this.getDataPanel().add(lblNr);
+		
+		JLabel lblPlz = new JLabel("Plz");
+		lblPlz.setBounds(10, 266, 75, 23);
+		this.getDataPanel().add(lblPlz);
+		
+		JLabel lblOrt = new JLabel("Ort");
+		lblOrt.setBounds(95, 266, 75, 23);
+		this.getDataPanel().add(lblOrt);
+		
+		JLabel lblBetrag = new JLabel("Zu zahlender Betrag");
+		lblBetrag.setBounds(477, 285, 149, 31);
+		this.getDataPanel().add(lblBetrag);
+		
+		this.setLblSumme(new JLabel("0.00"));
+		this.getLblSumme().setBounds(684, 285, 82, 31);
+		this.getDataPanel().add(this.getLblSumme());
+		
+		this.setBtnBackToCart(new JButton("Zurueck zum Warenkorb"));
+		this.getBtnBackToCart().setBounds(24, 482, 198, 23);
+		this.add(this.getBtnBackToCart());
+		
+		this.setBtnSendOrder(new JButton("Verbindlich bestellen"));
+		this.getBtnSendOrder().setBounds(785, 482, 198, 23);
+		this.add(this.getBtnSendOrder());
 		
 	}
 	
