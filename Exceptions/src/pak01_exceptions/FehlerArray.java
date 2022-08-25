1869e965397034213a50e4c4205a729db716ceae
Markus <Markus@MarkusVM.localdomain>
2022-08-25 09:34:09 +0200

    ...

---
+++ Exceptions/src/pak01_exceptions/FehlerArray.java
@@ -0,0 +1,31 @@
+package pak01_exceptions;
+
+public class FehlerArray {
+	
+	/*
+	 * Waehrend der normalen Abarbeitung einer Methode kann zur Laufzeit ein
+	 * abnormales Ereignis eintreten, das die normale Ausfuehrung der Methode unterbricht.
+	 * 
+	 * Ein solches abnormales Ereignis ist eine Exception (Ausnahme)
+	 * 
+	 * Eine Exception stellt damit ein Laufzeitereignis dar, das zum Versagen einer Methode
+	 * und damit zu einem Laufzeitfehler des Programms fuehren kann.
+	 */
+	
+	public static void main(String[] args) {
+		method01();
+	}
+	
+	static void method01() {
+		method02();
+	}
+	
+	static void method02() {
+		int[] zahlen = {1,2,3};
+		int summe = 0;
+		for (int i = 0; i <= zahlen.length; i++) {
+			summe += zahlen[i];
+		}
+		System.out.println("summe: " + summe);
+	}
+}
---
+++ Exceptions/src/pak03_throw_throws/Demo01Throw.java
@@ -0,0 +1,58 @@
+package pak03_throw_throws;
+
+public class Demo01Throw {
+	
+	/*
+	 * Mit throw kann eine Exception geworfen werden.
+	 * 
+	 * Alles, was von Throwable abgeleitet ist, kann mit dem Schluesselwort throw
+	 * geworfen werden.
+	 */
+	
+	public static void main(String[] args) {
+		
+//		throw
+//		throws
+//		int zahl = 1;
+//		
+//		if(zahl == 1) {
+//			throw new ArrayIndexOutOfBoundsException();			
+//		}
+		
+//		dividierenOhneTryCatch(1,0);
+		dividierenMitTryCatch(1, 0);
+		System.out.println("Programmende");
+	}
+	public static double dividierenMitTryCatch(double zahl1, double zahl2) {
+		double result = 0;
+		/*
+		 * Im Fehlerfall wird direkt in den Catchblock gesprungen,
+		 * der Rest vom Try Block wird nicht mehr abgearbeitet.
+		 */
+		try {
+			if(zahl2 == 0) {
+				throw new ArithmeticException("Teiler war 0");
+			}
+			System.out.println("Teiler war nicht 0");
+			result = zahl1/zahl2;			
+		}catch(ArithmeticException ae) {
+			ae.printStackTrace();
+		}
+		return result;
+		
+	}
+	public static double dividierenOhneTryCatch(double zahl1, double zahl2) {
+		double result = 0;
+		/*
+		 * Im Fehlerfall wird das Programm abgebrochen 
+		 * und das Ende der Methode nicht mehr erreicht.
+		 */
+		if(zahl2 == 0) {
+			throw new ArithmeticException("Teiler war 0");
+		}
+		System.out.println("Teiler war nicht 0");
+		result = zahl1/zahl2;
+		return result;
+		
+	}
+}
